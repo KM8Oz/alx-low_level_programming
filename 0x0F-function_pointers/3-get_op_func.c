be3906be272e009dcb5c0b21505070c9e934ac08
KM8Oz <ouazmir@gmail.com>
2023-04-19 23:10:18 -0700

    3 recursive

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,30 +1,46 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
+
 /**
- * get_op_func - gets the correct function for an operator
- * @s: operator to get correct function for
- *
- * Return: NULL if no match found, otherwise pointer to correct func
- */
+  * find_op_by_key - finds the operator
+  * @ops: the operator array
+  * @key: the operator
+  * @thisindex: the index of the operator
+  * Return: the operator func
+  */
+op_t find_op_by_key(op_t ops[], char *key, int thisindex)
+{
+if (key == NULL || thisindex >= 5 || strlen(key) > 1)
+{
+return (ops[5]);
+}
+if (*(ops[thisindex].op) == *key)
+{
+return (ops[thisindex]);
+}
+
+return (find_op_by_key(ops, key, thisindex + 1));
+
+}
+
+/**
+  * get_op_func - gets the operator function
+  * @s: the operator
+  *
+  * Return: result
+  */
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
 	};
-	int x = 0;
-
-	while (ops[x].op != NULL)
-	{
-	if (*(ops[x].op) == *s) /* found correct operator */
-	{
-		return (ops[x].f);
-	}
-	x++;
-	}
-	return (NULL); /* no match found */
+	return (find_op_by_key(ops, s, 0).f);
+	return (0);
 }
